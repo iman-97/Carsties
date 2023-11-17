489a6d3d15affe122d9ab3dff1f811a6fadc8742
iman-97 <iman1095@yahoo.com>
2023-11-17 14:57:33 +0330

    section 6 - episode 7

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,10 +1,25 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(o =>
+    {
+        o.Authority = builder.Configuration["IdentityServiceUrl"];
+        o.RequireHttpsMetadata = false;//because we are in http right now
+        o.TokenValidationParameters.ValidateAudience = false;
+        o.TokenValidationParameters.NameClaimType = "username";
+    });
+
 var app = builder.Build();
 
 app.MapReverseProxy();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.Run();
