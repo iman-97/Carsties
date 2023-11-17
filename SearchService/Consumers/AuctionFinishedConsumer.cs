3a4be94fecf8d957005a4656fbf23843392de961
iman-97 <iman1095@yahoo.com>
2023-11-17 18:33:33 +0330

    section 6 - end

---
+++ SearchService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,26 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+{
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        Console.WriteLine("--> Consuming Auction Finished");
+
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        if (context.Message.ItemSold == true)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = context.Message.Amount.Value;
+        }
+
+        auction.Status = "Finished";
+
+        await auction.SaveAsync();
+    }
+}
