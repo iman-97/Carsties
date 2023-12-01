c9268072918f648ea3ece1a507f73ddd6991775a
iman-97 <iman1095@yahoo.com>
2023-12-01 23:12:21 +0330

    section 7 - episode 8

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -29,6 +29,12 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, cfg) =>
     {
+        cfg.Host(builder.Configuration["RabbitMq:Host"],"/",h =>
+        {
+            h.Username(builder.Configuration.GetValue("RabbitMq:Username","guest"));
+            h.Password(builder.Configuration.GetValue("RabbitMq:Password","guest"));
+        });
+
         cfg.ConfigureEndpoints(context);
     });
 });
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -22,6 +22,12 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, cfg) =>
     {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", h =>
+        {
+            h.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            h.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+
         cfg.ReceiveEndpoint("search-auction-created", e =>
         {
             e.UseMessageRetry(r => r.Interval(5, 5));//retry the message 5 times every 5 seconds
